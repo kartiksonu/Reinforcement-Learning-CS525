68807e0d1a12c380befa967dc7bba28fdfd89578
kartiksonu <kartiksonu39@gmail.com>
2019-10-24 01:23:15 -0400

    added test params

--- RL_proj_3/agent_dqn.py
+++ RL_proj_3/agent_dqn.py
@@ -23,23 +23,26 @@ you can import any package and define any extra function as you need
 """
 #Hyper parameters for training
 
-REPLAY_SIZE = 1000000
+REPLAY_SIZE = 100000
 
 GAMMA = 0.99
 BATCH_SIZE = 32
         
 LEARNING_RATE = 1e-5
-TARGET_UPDATE_INTERVAL = 10000
+TARGET_UPDATE_INTERVAL = 1000
+#learning starts after 5000 experiences are collected
 LEARNING_STARTS = 5000
-SAVE_INTERVAL = 200
+
+#save model for every 1000 episodes
+SAVE_INTERVAL = 1000
 
 DEVICE = 'cuda'
 
-EPSILON_DECAY = 5000
+EPSILON_DECAY = 80000
 EPSILON_START = 1.0
 EPSILON_END   = 0.02
         
-LOAD_MODEL = False
+LOAD_MODEL = True
 MODEL = "breakoutNoFrameSkip.dat"
 
 Experience = collections.namedtuple('Experience', field_names=['state', 'action', 'reward', 'done', 'new_state'])
@@ -54,9 +57,7 @@ class Agent_DQN(Agent):
             initialize Q net and target Q net
             parameters for repaly buffer
             parameters for q-learning; decaying epsilon-greedy
-            ...
         """
-
         super(Agent_DQN,self).__init__(env)
         ###########################
         # initializations for replay memory
@@ -66,6 +67,9 @@ class Agent_DQN(Agent):
         #initializations of agent
         self._reset()
         self.last_action = 0
+        self.net = DQN((4, 84, 84), self.env.action_space.n).to(DEVICE)
+        self.target_net = DQN((4, 84, 84), self.env.action_space.n).to(DEVICE)
+        LOAD_MODEL = True
 
         
         if args.test_dqn:
@@ -73,7 +77,15 @@ class Agent_DQN(Agent):
             print('preparing to load trained model')
             ###########################
             LOAD_MODEL = True
+
+        
+        if LOAD_MODEL:
+            self.net.load_state_dict(torch.load(MODEL, map_location=lambda storage, loc: storage))
+            print('loaded trained model')
+            self.target_net.load_state_dict(self.net.state_dict())
             
+
+
 
     def init_game_setting(self):
         """
@@ -113,28 +125,45 @@ class Agent_DQN(Agent):
     def _reset(self):
         self.state = self.env.reset()
         self.total_reward = 0.0
-    
 
-    def make_action(self, net, epsilon=0.0, device='cuda'):
+
+
+    def make_action(self, observation, test= True):
         """
+        this is exclusively for testing our actions
         select action
         """
+        state_a_test = np.array([observation.transpose(2,0,1)], copy=False)
+        #torch.tensor opperation appends a '1' at the start of the numpy array
+        state_v_test = torch.tensor(state_a_test).to(DEVICE)
+        Q_values_v_test = self.net(state_v_test)
+        _, action_v_test = torch.max(Q_values_v_test, dim=1)
+        action_test = int(action_v_test.item())
+
+        ###########################
+        return action_test
+    
+
+    def make_action_train(self, net, epsilon=0.0, device=DEVICE):
+        """
+        select action for training purposes
+        """
         
-        if np.random.random() < epsilon:
+        if np.random.random() < self.epsilon:
             action = self.env.action_space.sample()
 
         else:
             state_a = np.array([self.state.transpose(2,0,1)], copy=False)
             #torch.tensor opperation appends a '1' at the start of the numpy array
             state_v = torch.tensor(state_a).to(device)
-            Q_values_v = net(state_v)
+            Q_values_v = self.net(state_v)
             _, action_v = torch.max(Q_values_v, dim=1)
             action = int(action_v.item())
 
         ###########################
         return action
 
-    def play_step(self, net, epsilon=0.0, device='cuda'):
+    def play_step(self, net, epsilon=0.0, device=DEVICE):
 
         """
         execute action and take a step in the environment
@@ -142,7 +171,7 @@ class Agent_DQN(Agent):
         return the total_reward
         """
         done_reward = None
-        action_for_exp = self.make_action(net, epsilon, DEVICE)
+        action_for_exp = self.make_action_train(self.net, self.epsilon, DEVICE)
 
         new_state, reward, is_done, _ = self.env.step(action_for_exp)
         self.total_reward += reward
@@ -165,7 +194,7 @@ class Agent_DQN(Agent):
         return done_reward
 
 
-    def loss_function(self, batch, net, target_net, device="gpu"):
+    def loss_function(self, batch, net, target_net, device=DEVICE):
 
         states, actions, rewards, dones, next_states = batch
 
@@ -175,8 +204,8 @@ class Agent_DQN(Agent):
         rewards_v = torch.tensor(rewards).to(device)
         done = torch.BoolTensor(dones).to(device)
 
-        state_action_values =net(states_v).gather(1, actions_v.long().unsqueeze(-1)).squeeze(-1)
-        next_state_values  = target_net(next_states_v).max(1)[0]
+        state_action_values =self.net(states_v).gather(1, actions_v.long().unsqueeze(-1)).squeeze(-1)
+        next_state_values  = self.target_net(next_states_v).max(1)[0]
         next_state_values[done] = 0.0
         next_state_values = next_state_values.detach()
 
@@ -193,15 +222,13 @@ class Agent_DQN(Agent):
         Implement your training algorithm here
         """
         ###########################
-        device = torch.device("cuda")
+        device = torch.device(DEVICE)
+        
 
-        net = DQN((4, 84, 84), self.env.action_space.n).to(device)
-        target_net = DQN((4, 84, 84), self.env.action_space.n).to(device)
-
-        epsilon = EPSILON_START
+        self.epsilon = EPSILON_START
 
         #defining the optimizer for your neural network
-        optimizer = optim.RMSprop(net.parameters(), lr=LEARNING_RATE)
+        optimizer = optim.RMSprop(self.net.parameters(), lr=LEARNING_RATE)
 
         #empty list of total rewards
         total_rewards = []
@@ -210,19 +237,13 @@ class Agent_DQN(Agent):
         frame_idx = 0
         timestep_frame = 0
         timestep = time.time()
-        
-        if LOAD_MODEL:
-            net.load_state_dict(torch.load(MODEL, map_location=lambda strorage, loc: storage))
-            print('loaded trained model')
-            target_net.load_state_dict(net.state_dict())
-            
 
         while True:
 
             frame_idx += 1
-            epsilon = max(EPSILON_END, EPSILON_START-frame_idx/EPSILON_DECAY)
+            self.epsilon = max(EPSILON_END, EPSILON_START-frame_idx/EPSILON_DECAY)
 
-            reward = self.play_step(net, epsilon, device=device)
+            reward = self.play_step(self.net, self.epsilon, device=device)
 
             if reward is not None:
                 #appending rewards in an empty list of total_rewards
@@ -233,10 +254,10 @@ class Agent_DQN(Agent):
                 timestep_frame = frame_idx
                 timestep = time.time()
 
-                #calculating mean of last(recent) 100 rewards
-                mean_reward = np.mean(total_rewards[-100:])
+                #calculating mean of last(recent) 30 rewards
+                mean_reward = np.mean(total_rewards[-30:])
 
-                print("{} frames: done {} games, mean reward {}, epsilon {}, speed {} frames/s".format(frame_idx, len(total_rewards), round(mean_reward, 3), round(epsilon,2), round(speed, 2)))
+                print("{} frames: done {} games, mean reward {}, epsilon {}, speed {} frames/s".format(frame_idx, len(total_rewards), round(mean_reward, 3), round(self.epsilon,2), round(speed, 2)))
 
                 if best_mean_reward is None or best_mean_reward < mean_reward or len(total_rewards)%25 == 0:
                     
@@ -245,7 +266,7 @@ class Agent_DQN(Agent):
 
 
             if frame_idx % SAVE_INTERVAL == 0:
-                torch.save(net.state_dict(), "breakoutNoFrameSkip" + "-" +" str(len(total_rewards))" + ".dat")
+                torch.save(self.net.state_dict(), 'breakoutNoFrameSkip'+'.dat')
 
             #checking the replay memory
             if len(self.buffer) < LEARNING_STARTS:
@@ -253,7 +274,7 @@ class Agent_DQN(Agent):
 
             #check if we need to update our target function
             if frame_idx % TARGET_UPDATE_INTERVAL == 0:
-                target_net.load_state_dict(net.state_dict())
+                self.target_net.load_state_dict(self.net.state_dict())
 
             # we dont wanna accumilate our gradients
             # hence it is importent to make them zero at every iteration
@@ -261,7 +282,7 @@ class Agent_DQN(Agent):
             optimizer.zero_grad()
             # sampling a batch from buffer
             batch = self.replay_buffer(BATCH_SIZE)
-            loss_t = self.loss_function(batch, net, target_net, device)
+            loss_t = self.loss_function(batch, self.net, self.target_net, device)
             #printing loss at every 100 episodes
             if len(total_rewards) % 100  == 0:
                 print("loss at episode"+str(len(total_rewards))+"is")
